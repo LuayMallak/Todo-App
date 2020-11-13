f2879851fa80bf4b73cb70c615516073290e6e49
Luay Mallak <luay.mallak@gmail.com>
2020-11-13 15:31:31 +0100

    Updates

---
+++ precache-manifest.38568f172aa953b61ae2e96af3152299.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffff8a1b06208f2bb38a5d0f05cdb40f",
+    "url": "/SPA-todo-app/index.html"
+  },
+  {
+    "revision": "a30f47e8e64ee7fec21a",
+    "url": "/SPA-todo-app/static/css/main.880c4b8c.chunk.css"
+  },
+  {
+    "revision": "cff12cd587da29a233ee",
+    "url": "/SPA-todo-app/static/js/2.f4c9fba0.chunk.js"
+  },
+  {
+    "revision": "1e8c639be0d055b48b146f697441b021",
+    "url": "/SPA-todo-app/static/js/2.f4c9fba0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a30f47e8e64ee7fec21a",
+    "url": "/SPA-todo-app/static/js/main.332f45c5.chunk.js"
+  },
+  {
+    "revision": "609caa454de732c6bfd0",
+    "url": "/SPA-todo-app/static/js/runtime-main.db33ed8a.js"
+  }
+]);
